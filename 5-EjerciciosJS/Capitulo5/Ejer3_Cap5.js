5dda720cf1836f88ab5e13cc8a530aa0a07c367f
LeonardoBrabo <leonardo.david.brabo@gmail.com>
2022-07-26 12:10:32 -0300

    Ejer 3 cap5 arrlegado

--- 5-EjerciciosJS/Capitulo5/Ejer3_Cap5.js
+++ 5-EjerciciosJS/Capitulo5/Ejer3_Cap5.js
@@ -1,18 +1,7 @@
 // 17) Programa una función que dada una fecha válida determine cuantos años han pasado hasta el día de hoy, pe. miFuncion(new Date(1984,4,23)) devolverá 35 años (en 2020).
 
-/*
-const aniosPasados = (anio,mes,dia) =>{
-    mes--;
 
-    let fechaActual = Date.now()
-    let fechaIngresada = Date.parse(new Date(anio,mes,dia));
-    let resta = fechaActual-fechaIngresada
-    return console.log(resta);
-
-}
-
-aniosPasados(1995,11,28);
-*/
+//Version 1:
 
 const getEdad = (fecha)=>{
     let fechaActual = new Date();
@@ -32,4 +21,17 @@ const getEdad = (fecha)=>{
     return console.log(`${edad} años, hoy ${fechaActual.toLocaleDateString()}`);
 }
 
-getEdad("1995/11/28")
+
+getEdad("1995/11/28");
+
+
+//version 2:
+function calcularEdad(fecha){
+    let diferencia = Date.now() - fecha.getTime();
+
+    fecha = new Date(diferencia);
+
+    return Math.abs(fecha.getUTCFullYear() - 1970);
+}
+
+console.log(calcularEdad(new Date(1995,11,28)));
